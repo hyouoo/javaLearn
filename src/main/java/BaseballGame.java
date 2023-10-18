7dfc989f11f79a013fce44f899c500d7e1c2bad7
hyouoo <hyouoo@gmail.com>
2023-10-18 17:36:49 +0900

    baseballgame add

---
+++ src/main/java/BaseballGame.java
@@ -0,0 +1,64 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class BaseballGame {
+    public static void main(String[] args) {
+        //computer 는 0과 9 사이의 서로 다른 숫자 3개를 무작위로 뽑는다.
+        Random random = new Random();
+        int num1 = random.nextInt(10);
+        int num2, num3;
+        while (true) {
+            num2 = random.nextInt(10);
+            num3 = random.nextInt(10);
+            if (num1 == num2 || num2 == num3 || num3 == num1) {
+                continue;
+            }
+            break;
+        }
+        String outNum = String.valueOf(num1 * 100 + num2 * 10 + num3);
+
+        //게임이 종료될 때까지 사용자의 입력을 받는다
+        System.out.println("컴퓨터가 숫자를 생성하였습니다. 답을 맞춰보세요!");
+        Scanner sc = new Scanner(System.in);
+        int trialNum = 1;
+        String result;
+        while (true) {
+            String trialStr = String.format("%d번째 시도 : ", trialNum);
+            System.out.print(trialStr);
+            String trial = sc.next();
+            //두 개의 숫자를 비교하고 결과를 출력한다, 3개가 맞으면 게임을 종료한다
+            int strike = 0;
+            int ball = 0;
+            for (char c : trial.toCharArray()) {
+                if (outNum.indexOf(c) != -1) {
+                    if (trial.indexOf(c) == outNum.indexOf(c)) {
+                        strike++;
+                    } else {
+                        ball++;
+                    }
+                }
+            }
+            // 비교 결과에 따른 출력 양식을 선택
+            if (strike == 0) {
+                if (ball == 0) {
+                    result = String.format("%dB%S", ball, strike);
+                } else {
+                    result = String.format("%dB", ball);
+                }
+            } else if (ball == 0) {
+                result = String.format("%dS", strike);
+            } else {
+                result = String.format("%dB%dS", ball, strike);
+            }
+            System.out.println(result);
+            // 게임 종료
+            if (strike == 3) {
+                String endResult = String.format("%d번만에 맞히셨습니다.", trialNum);
+                System.out.println(endResult);
+                System.out.println("게임을 종료합니다.");
+                break;
+            }
+            trialNum++;
+        }
+    }
+}
