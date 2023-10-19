72ac51afeb856efa924856c24436134087fd20bc
hyouoo <hyouoo@gmail.com>
2023-10-19 14:49:55 +0900

    fix AddOperation in W4HW

--- src/main/java/WeekFourHW/AddOperation.java
+++ src/main/java/WeekFourHW/AddOperation.java
@@ -3,6 +3,6 @@ package WeekFourHW;
 public class AddOperation extends AbstractOperation {
     @Override
     public double operate(int firstNumber, int secondNumber) {
-        return 3 + 5;
+        return firstNumber + secondNumber;
     }
 }
