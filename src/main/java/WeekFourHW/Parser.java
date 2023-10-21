3b69040d1fb00ad515a05d879b48648033c56b3e
Sanghyu Lee <hyouoo@gmail.com>
2023-10-22 01:26:06 +0900

    code fix

--- src/main/java/WeekFourHW/Parser.java
+++ src/main/java/WeekFourHW/Parser.java
@@ -6,13 +6,12 @@ public class Parser {
 
     private final Calculator calculator = new Calculator();
 
-    public Parser parseFirstNum(String firstInput) throws BadInputException {
+    public void parseFirstNum(String firstInput) throws BadInputException {
         // 구현 1.
         if (!firstInput.matches(NUMBER_REG)) {
             throw new BadInputException("정수");
         }
         this.calculator.setFirstNumber(Integer.parseInt(firstInput));
-        return this;
     }
 
     public void parseSecondNum(String secondInput) throws BadInputException {
