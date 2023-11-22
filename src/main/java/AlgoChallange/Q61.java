7414656dfccf73e4627b5f274071af9141161a0a
Sanghyu Lee <hyouoo@gmail.com>
2023-11-22 09:23:12 +0900

    ~Q62 solve

--- src/main/java/AlgoChallange/Q61.java
+++ src/main/java/AlgoChallange/Q61.java
@@ -16,8 +16,7 @@ public class Q61 {
         String today = "2020.01.01";
         String[] terms = {"Z 3", "D 5"};
         String[] privacies = {"2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"};
-        System.out.println(Arrays.toString(solution(today, terms, privacies)));
-
+        System.out.println(Arrays.toString(solution2(today, terms, privacies)));
     }
 
     // privacies의 날짜에 코드별 유효기한을 더한 날짜를 확인하고 오늘 날짜와 비교해서 폐기 여부 결정
@@ -47,7 +46,7 @@ public class Q61 {
                     newMonth = date[1] + Integer.parseInt(spliTerm[1]);
                     newYear = date[0] + newMonth / 12;
                     newMonth = ((newMonth - 1) % 12) + 1;
-                    if (date[2] - 1 == 0){
+                    if (date[2] - 1 == 0) {
                         if (newMonth - 1 == 0) {
                             newYear--;
                             newMonth = 12;
@@ -55,7 +54,7 @@ public class Q61 {
                             newMonth--;
                         }
                         newDay = 28;
-                    }else {
+                    } else {
                         newDay = date[2] - 1;
                     }
                     LocalDate endDate = LocalDate.of(newYear, newMonth, newDay);
@@ -71,5 +70,47 @@ public class Q61 {
             answer[i] = lst.get(i);
         }
         return answer;
+    }
+
+    // 2000년 1월 1일을 day 0으로 보관이 시작된 날에서 기한까지 더한 후에 연도로 변환
+    // 1개월이면 28이고, 1/28일을 반환 -> 1개월 : 28 : 2000.01.28, 2개월 > 56 > 2000.02.28
+    public static int[] solution2(String today, String[] terms, String[] privacies) {
+        int daysCollect = 0;
+        int daysDispose = 0;
+        int daysToday = calculateDays(today);
+        List<Integer> lst = new ArrayList<>();
+
+        for (int i = 0; i < privacies.length; i++) {
+            String[] splitPrivacy = privacies[i].split(" ");
+            daysCollect = calculateDays(splitPrivacy[0]);
+
+            for (String term : terms) {
+                String[] splitTerm = term.split(" ");
+                if (splitPrivacy[1].equals(splitTerm[0])) {
+                    daysDispose = daysCollect + Integer.parseInt(splitTerm[1]) * 28;
+                    break;
+                }
+            }
+
+            if (daysDispose < daysToday) {
+                lst.add(i + 1);
+            }
+        }
+
+        int[] answer = new int[lst.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = lst.get(i);
+        }
+        return answer;
+    }
+
+    public static int calculateDays(String day) {
+        String[] split = day.split("\\.");
+        int[] toInt = new int[split.length];
+
+        for (int i = 0; i < toInt.length; i++) {
+            toInt[i] = Integer.parseInt(split[i]);
+        }
+        return (toInt[0] - 2000) * 28 * 12 + (toInt[1] - 1) * 28 + toInt[2];
     }
 }
--- src/main/java/AlgoChallange/Q62.java
+++ src/main/java/AlgoChallange/Q62.java
@@ -1,6 +1,9 @@
 package AlgoChallange;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 // 달리기 경기
 // https://school.programmers.co.kr/learn/courses/30/lessons/178871
@@ -11,10 +14,11 @@ public class Q62 {
     public static void main(String[] args) {
         String[] players = {"mumu", "soe", "poe", "kai", "mine"};
         String[] callings = {"kai", "kai", "mine", "mine"};
-        System.out.println(Arrays.toString(solution(players, callings)));
+        System.out.println(Arrays.toString(solution3(players, callings)));
     }
 
     private static String[] solution(String[] players, String[] callings) {
+
         StringBuilder sb = new StringBuilder();
         for (String calling : callings) {
             for (int j = 0; j < players.length; j++) {
@@ -27,5 +31,54 @@ public class Q62 {
             }
         }
         return players;
+    }
+
+    // keys 배열의 연산을 통해서 index의 최종 순위 결정
+    private static String[] solution2(String[] players, String[] callings) {
+        int[] keys = new int[players.length];
+        String[] result = players;
+
+        for (String calling : callings) {
+            for (int i = 0; i < players.length; i++) {
+                if (calling.equals(result[i])) {
+                    keys[i] -= 1;
+                    keys[i + keys[i]] += 1;
+                    result[i + keys[i]] = players[i];
+                    break;
+                }
+            }
+        }
+
+//        for (int i = 0; i < result.length; i++) {
+//            result[i - keys[i]] = players[i];
+//        }
+        return result;
+    }
+
+    private static String[] solution3(String[] players, String[] callings) {
+        Map<String, Integer> rank = new HashMap<>();
+        Map<Integer, String> result = new HashMap<>();
+
+        String[] finish = new String[players.length];
+
+        for (int i = 0; i < players.length; i++) {
+            rank.put(players[i], i);
+            result.put(i, players[i]);
+        }
+
+        for (String call : callings) {
+            String temp = result.get(rank.get(call) - 1);
+            rank.replace(call, rank.get(call) - 1);
+            rank.replace(temp, rank.get(call) + 1);
+
+            result.replace(rank.get(call), call);
+            result.replace(rank.get(call) + 1, temp);
+        }
+
+        for (Entry<Integer, String> entry : result.entrySet()) {
+            finish[entry.getKey()] = entry.getValue();
+        }
+
+        return finish;
     }
 }
